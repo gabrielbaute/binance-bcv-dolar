a174a68aab1280869e4b31debb34c1fe44dd78af
gabrielbaute <gabrielbaute@gmail.com>
2025-11-15 16:28:54 -0400

    Rebuild of binance response

--- app/schemas/binance_response_schemas.py
+++ app/schemas/binance_response_schemas.py
@@ -1,40 +1,20 @@
 from typing import List, Optional, Any
 from pydantic import BaseModel
 
-class TradeMethod(BaseModel):
-    payType: str
-    identifier: str
-    tradeMethodName: Optional[str]
-    tradeMethodShortName: Optional[str]
-
-class Adv(BaseModel):
-    advNo: str
-    tradeType: str
-    asset: str
-    fiatUnit: str
-    price: str
-    surplusAmount: Optional[str]
-    tradableQuantity: Optional[str]
-    minSingleTransAmount: Optional[str]
-    maxSingleTransAmount: Optional[str]
-    tradeMethods: List[TradeMethod]
-    isTradable: bool
-
-class Advertiser(BaseModel):
-    nickName: str
-    monthOrderCount: Optional[int]
-    monthFinishRate: Optional[float]
-    positiveRate: Optional[float]
-    userType: str
-
-class DataItem(BaseModel):
-    adv: Adv
-    advertiser: Advertiser
-
 class BinanceResponse(BaseModel):
-    code: str
-    message: Optional[str]
-    messageDetail: Optional[str]
-    data: List[DataItem]
-    total: Optional[int]
-    success: bool
+    """
+    Schema for the response from Binance P2P.
+
+    Keyword arguments:
+        fiat (str): Fiat currency.
+        asset (str): Asset (USDT, BTC, etc).
+        prices (List[float]): List of prices.
+        average_price (Optional[float]): Average price.
+        median_price (Optional[float]): Median price.
+    """
+    fiat: str
+    asset: str
+    trade_type: str
+    prices: List[float]
+    average_price: Optional[float] = None
+    median_price: Optional[float] = None
