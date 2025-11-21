8cd3c76cc5b70ba96e3a7e4cd583e65e6d196883
gabrielbaute <gabrielbaute@gmail.com>
2025-11-20 23:03:10 -0400

    Instance dir for database location

--- app/config/config.py
+++ app/config/config.py
@@ -1,3 +1,6 @@
+"""
+General configuration for the app.
+"""
 import os
 from dotenv import load_dotenv
 
@@ -7,7 +10,9 @@ class Config:
     """
     Config class for environtment variables.
     """
+    # Directory and path config
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    INSTANCE_DIR = os.getenv("INSTANCE_DIR", os.path.join(BASE_DIR, "..", "instance"))
     
     # Logging config
     LOG_DIR = os.getenv("LOG_DIR", os.path.join(BASE_DIR, "logs"))
@@ -18,5 +23,5 @@ class Config:
     API_HOST = os.getenv("API_HOST", "0.0.0.0")
 
     # Database config
-    DATABASE_URL = "sqlite:///./exchange_rates.db"
+    DATABASE_URL = f"sqlite:///{os.path.join(INSTANCE_DIR, 'exchange_rates.db')}"
     DATABASE_CONNECT_ARGS = {"check_same_thread": False}
