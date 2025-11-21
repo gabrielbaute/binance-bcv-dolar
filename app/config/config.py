18461f20b04deae58c51f3635f168801a29bf395
gabrielbaute <gabrielbaute@gmail.com>
2025-11-21 17:57:58 -0400

    Webhooks variables

--- app/config/config.py
+++ app/config/config.py
@@ -11,6 +11,9 @@ class Config:
     """
     Config class for environtment variables.
     """
+    APP_NAME = "P2P Exchange Tracker"
+    APP_VERSION = "0.1.0"
+
     # Directory and path config
     BASE_DIR = Path(__file__).resolve().parent
     INSTANCE_DIR = Path(os.getenv("INSTANCE_DIR", BASE_DIR / ".." / "instance"))
@@ -21,9 +24,13 @@ class Config:
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
     
     # API config
-    API_PORT = os.getenv("API_PORT", 8000)
+    API_PORT = int(os.getenv("API_PORT", 8000))
     API_HOST = os.getenv("API_HOST", "0.0.0.0")
 
     # Database config
     DATABASE_URL = f"sqlite:///{os.path.join(INSTANCE_DIR, 'exchange_rates.db')}"
     DATABASE_CONNECT_ARGS = {"check_same_thread": False}
+
+    # Webhooks
+    NTFY_TOPIC = os.getenv("NTFY_TOPIC", "p2p_tracker_alerts")
+    NTFY_URL = os.getenv("NTFY_URL", "https://ntfy.sh")
