b45d2d48a00b2d4e12be2c7bd2eba4b497f6e39f
gabrielbaute <gabrielbaute@gmail.com>
2025-11-21 07:48:58 -0400

    Dolar Venezuela service added to module

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,6 @@
 from app.services.binance_p2p import BinanceP2P
 from app.services.bcv_scrapper import BCVScraper
 from app.services.fiat_exchange_service import FiatExchengeService
+from app.services.average_dolar_service import DolarService
 
-__all__ = ["BinanceP2P", "BCVScraper", "FiatExchengeService"]
+__all__ = ["BinanceP2P", "BCVScraper", "FiatExchengeService", "DolarService"]
