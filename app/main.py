2b68ca742917e81ab037bee9c92b60afaf9d7e79
gabrielbaute <gabrielbaute@gmail.com>
2025-12-02 09:29:27 -0400

    From on_event to lifespan method in fastapi

--- app/main.py
+++ app/main.py
@@ -10,7 +10,21 @@ from app.database.db_config import init_db
 
 setup_logging(log_dir=str(Config.LOG_DIR))
 
-app = FastAPI(title="P2P Exchange Rate API")
+# Definir lifespan
+async def lifespan(app: FastAPI):
+    # --- Startup ---
+    # Inicializar DB
+    init_db(instance_dir=Config.INSTANCE_DIR)
+
+    # Inicializar scheduler
+    scheduler = DolarScheduler()
+    scheduler.start()
+
+    # yield mantiene la app viva hasta que se cierre
+    yield
+
+# Crear app con lifespan
+app = FastAPI(title="P2P Exchange Rate API", lifespan=lifespan)
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="app/ui/static"), name="static")
@@ -22,15 +36,6 @@ include_routers(app)
 @app.get("/")
 async def read_index():
     return FileResponse('app/ui/index.html')
-
-# Init database
-init_db(instance_dir=Config.INSTANCE_DIR)
-
-# Init scheduler 
-@app.on_event("startup")
-def startup_event():
-    scheduler = DolarScheduler()
-    scheduler.start()
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host=Config.API_HOST, port=int(Config.API_PORT), reload=True)
