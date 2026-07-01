5a72b9cb9f25f59e413f5ae635eed2fd2a614690
gabrielbaute <gabrielbaute@gmail.com>
2026-07-01 17:01:15 -0400

    Refactorizamos las rutas de fiat. Cambiamos el prefijo a arbitrage, que es mas específico de lo que hace la ruta y los recursos que entrega. Se dividió en dos rutas: una para las rutas real-time que van directo a Binance, y otra para las consultas a la base de datos.

--- app/api/routes/fiat_routes.py
+++ app/api/routes/fiat_routes.py
@@ -1,17 +1,34 @@
-# app/api/routes/fiat_routes.py
-from fastapi import APIRouter, Query
-from app.services.fiat_exchange_service import FiatExchengeService
+from fastapi import APIRouter, Query, Depends
+
+from app.enums import FiatCurrency
+from app.services import FiatExchengeService
+from app.api.dependencies import get_fiat_exchenge_service
 from app.schemas.fiats_pair_response import FiatPairResponse
 
-router = APIRouter(prefix="/fiat", tags=["Remesas"])
-fiat_service = FiatExchengeService()
+router = APIRouter(prefix="/arbitrage", tags=["Remesas/Arbitraje"])
 
-@router.get("/pair", response_model=FiatPairResponse, summary="Get fiat/fiat pair")
-def get_fiat_pair(
+@router.get("/pair", response_model=FiatPairResponse, summary="Get fiat/fiat pair from last database record.")
+async def get_fiat_pair(
     fiat_1: str = Query(..., description="First fiat currency, e.g. VES"),
-    fiat_2: str = Query(..., description="Second fiat currency, e.g. PEN")
+    fiat_2: str = Query(..., description="Second fiat currency, e.g. PEN"),
+    exchenge_service: FiatExchengeService = Depends(get_fiat_exchenge_service)
 ):
     """
     Returns the average exchange rate for the selected pair. This route is used to calculate estimated remittance prices. It requires the currency of both countries and calculates rates in both directions.
     """
-    return fiat_service.get_pair(fiat_1=fiat_1, fiat_2=fiat_2)
+    fiat_1_value = FiatCurrency.from_string(fiat_1)
+    fiat_2_value = FiatCurrency.from_string(fiat_2)
+    return await exchenge_service.get_pair(fiat_1_value, fiat_2_value)
+
+@router.get("/real_time_pair", response_model=FiatPairResponse, summary="Get fiat/fiat pair fetching prices direct from Binance.")
+def get_real_time_pair(
+    fiat_1: str = Query(..., description="First fiat currency, e.g. VES"),
+    fiat_2: str = Query(..., description="Second fiat currency, e.g. PEN"),
+    exchenge_service: FiatExchengeService = Depends(get_fiat_exchenge_service)   
+):
+    """
+    Returns the average exchange rate for the selected pair. No database query, fetch direct from Binance and calculate rates.
+    """
+    fiat_1_value = FiatCurrency.from_string(fiat_1)
+    fiat_2_value = FiatCurrency.from_string(fiat_2)
+    return exchenge_service.get_real_time_pair(fiat_1_value, fiat_2_value)
