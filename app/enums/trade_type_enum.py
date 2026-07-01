2172078182743e8dca430096e47b894ad94f8c06
gabrielbaute <gabrielbaute@gmail.com>
2026-07-01 16:29:51 -0400

    Agregamos el método from_string al enum de TradeType y usamos List en el tipado del método list_trades

--- app/enums/trade_type_enum.py
+++ app/enums/trade_type_enum.py
@@ -1,3 +1,4 @@
+from typing import List
 from enum import StrEnum
 
 class TradeType(StrEnum):
@@ -18,9 +19,21 @@ class TradeType(StrEnum):
         return self.value
     
     @staticmethod
-    def list_trades() -> list:
-        return [trade.value for trade in TradeType]
+    def list_trades() -> List['TradeType']:
+        trade_list = [
+            TradeType.BUY,
+            TradeType.SELL
+        ]
+        return trade_list
     
     @staticmethod
     def is_valid_trade(trade: str) -> bool:
         return trade in TradeType.list_trades()
+    
+    @staticmethod
+    def from_string(trade_type_str: str) -> 'TradeType':
+        """Validate the strings and returns an TradeType object."""
+        try:
+            return TradeType(trade_type_str)
+        except ValueError:
+            raise ValueError(f"Invalid trade type value: {trade_type_str}. Valid options are: {', '.join(TradeType.list_trades())}")
