15bba3e7d4903a4c6d90eb8d6d839fb2325af3b2
gabrielbaute <gabrielbaute@gmail.com>
2026-07-01 18:10:21 -0400

    Pasamos string vacio al prefijo por defecto e incluimos la ruta de archivos estaticos

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,2 @@
+from app.api.app_factory import create_app
 from app.api.include_routers import include_routers
--- app/api/include_routers.py
+++ app/api/include_routers.py
@@ -6,9 +6,10 @@ from app.api.routes.binance_routes import router as binance_router
 from app.api.routes.history_routes import router as history_router
 from app.api.routes.fiat_routes import router as fiat_router
 from app.api.routes.health_routes import router as health_router
+from app.api.routes.ui_routes import router as ui_router
 
 
-def include_routers(app: FastAPI, prefix: str):
+def include_routers(app: FastAPI, prefix: str = ""):
     """
     Include routers for API routes
     """
@@ -18,3 +19,4 @@ def include_routers(app: FastAPI, prefix: str):
     app.include_router(history_router, prefix=prefix)
     app.include_router(fiat_router, prefix=prefix)
     app.include_router(health_router, prefix=prefix)
+    app.include_router(ui_router)
