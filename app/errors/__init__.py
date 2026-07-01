bd1bf41b0040098d9c7fdcd3ed050253f7b22d15
gabrielbaute <gabrielbaute@gmail.com>
2026-07-01 08:48:58 -0400

    Agregamos el error DatabaseSessionError para cuando se ejecute el servicio en inyección de dependnecias, se corrigen detalles semánticos en BCVReadingRateError

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -1,2 +1,8 @@
 from app.errors.base_error import DolarVzlaError
-from app.errors.app_errors import BCVConnectionError, BinanceConnectionError, RegisterNotFoundError
+from app.errors.app_errors import (
+    BCVConnectionError, 
+    BinanceConnectionError, 
+    RegisterNotFoundError,
+    DatabaseSessionError,
+    BCVReadingRateError
+)
--- app/errors/app_errors.py
+++ app/errors/app_errors.py
@@ -4,6 +4,11 @@ class BCVConnectionError(DolarVzlaError):
     """Error raised when there is a connection issue with the BCV website."""
     def __init__(self, message: str = "Error connecting to the BCV website."):
         super().__init__(message)
+    
+class BCVReadingRateError(DolarVzlaError):
+    """Error raised when there is an issue reading the rate from the BCV website."""
+    def __init__(self, message: str = "Error reading the rate from the BCV website."):
+        super().__init__(message)
 
 class BinanceConnectionError(DolarVzlaError):
     """Error raised when there is a connection issue with the Binance P2P API."""
@@ -13,4 +18,9 @@ class BinanceConnectionError(DolarVzlaError):
 class RegisterNotFoundError(DolarVzlaError):
     """Error raised when a rate register is not found on the database."""
     def __init__(self, message: str = "Register not found"):
+        super().__init__(message)
+
+class DatabaseSessionError(DolarVzlaError):
+    """Error raised when there is an issue with the database session."""
+    def __init__(self, message: str = "Database session error"):
         super().__init__(message)
